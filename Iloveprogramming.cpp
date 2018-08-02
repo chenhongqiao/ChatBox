fefb86542d3d7b19add874eb0ce1fdac04dd1b7a
chenhongqiao <1020060314@qq.com>
2018-08-02 08:09:16 -0500

    initial commit

--- Iloveprogramming.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include<windows.h>
-using namespace std;
-int main(){
-    cout<<"I love programming."<<endl;
-    return 0;
-}
--- love_programming.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<windows.h>
-using namespace std;
-int main(){
-    cout<<"I love programming."<<endl;
-    system("pause");
-    return 0;
-}
